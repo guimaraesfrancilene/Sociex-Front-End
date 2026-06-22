ac4cfcb212525f30fe9bc4001fb40030ead00c8c
Francilene Guimarães <francileneguimaraes37@gmail.com>
2026-06-22 19:36:54 -0300

    Ajustes

--- src/pages/composto/VisaoGeralProjeto.jsx
+++ src/pages/composto/VisaoGeralProjeto.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import "./VisaoGeralProjeto.css";
+import imgMenu from '../../assets/menulateral.png'
+import { useNavigate } from 'react-router-dom'
 
 const projetosData = [
   { id: 1, titulo: "Gestão de Rebanho", categoria: "Agropecuária", data: "25Nov/2025" },
@@ -12,13 +14,6 @@ const projetosData = [
   { id: 8, titulo: "Smart Study", categoria: "Educação", data: "25Nov/2025" },
 ];
 
-const IconeLogo = () => (
-  <div className="logo-icon">
-    <div></div><div></div>
-    <div></div><div></div>
-    <div></div><div></div><div></div>
-  </div>
-);
 
 const IconeLupa = () => (
   <svg fill="none" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24">
@@ -44,7 +39,7 @@ const IconeX = () => (
 export default function VisaoGeralProjetos() {
   const [filtroAtivo, setFiltroAtivo] = useState("Todos");
   const [busca, setBusca] = useState("");
-
+  const navigate = useNavigate()
   const projetosFiltrados = projetosData.filter(p => 
     p.titulo.toLowerCase().includes(busca.toLowerCase())
   );
@@ -54,14 +49,14 @@ export default function VisaoGeralProjetos() {
       <div className="topbar">
         <div className="header-top">
           <div className="logo-title">
-            <IconeLogo />
+            <img src={imgMenu} alt="Menu" style={{ height: '40px' }} />
             <div>
               <div className="titulo">Visão Geral de Projetos</div>
               <div className="subtitulo">(10 pendentes)</div>
             </div>
           </div>
           <nav className="nav">
-            <a href="#">Início</a>
+            <span onClick={() => navigate('/universitario/dashboard')}>Início</span>
             <a href="#">Sobre nós</a>
           </nav>
         </div>
